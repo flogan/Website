a9f8cf981c1a8a82eb69d0a6b7742eefc3ee6cce
flogan <flogan@umass.edu>
2019-01-23 18:51:54 -0500

    functionality and style additions

---
+++ index.js
@@ -0,0 +1,13 @@
+function toggle(id) {
+var txt = document.getElementById(id);
+
+var tabs = document.getElementsByClassName("tab");
+console.log(tabs)
+for (i = 0; i < tabs.length; i++) {
+    tabs[i].style.display = "none";
+}
+
+console.log("here now")
+txt.style.display = "block";
+console.log(txt)
+}
